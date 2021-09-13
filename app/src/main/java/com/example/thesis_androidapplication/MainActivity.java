6606afe415aee15f826b3c39d1d8adfe4a758d56
Irvin Gil <theofficial.irvingil@gmail.com>
2021-09-13 11:08:30 +0800

    initial commit

---
+++ app/src/main/java/com/example/thesis_androidapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.thesis_androidapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void manageMethod(View manageButton){
+        Intent i =new Intent(this, secondActivity.class); //initializing the intent
+        startActivity(i);  //start the next activity
+    }
+}
