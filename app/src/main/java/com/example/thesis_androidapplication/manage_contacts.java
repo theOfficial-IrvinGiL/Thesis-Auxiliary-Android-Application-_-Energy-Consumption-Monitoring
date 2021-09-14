2f2bd165dffec7e6b0cff6e299bb2da9a2a9cc14
Irvin Gil <theofficial.irvingil@gmail.com>
2021-09-15 05:22:41 +0800

    added the add contact page

--- app/src/main/java/com/example/thesis_androidapplication/manage_contacts.java
+++ app/src/main/java/com/example/thesis_androidapplication/manage_contacts.java
@@ -2,7 +2,11 @@ package com.example.thesis_androidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class manage_contacts extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class manage_contacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_contacts);
+
+
+        //code for floating action button
+        /*
+        * color of the floating action button can be modified on the colors.xml
+        * */
+        FloatingActionButton toAddcontacts = findViewById(R.id.addContactsFAbutton);
+         toAddcontacts.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent thisIntent =new Intent(manage_contacts.this, add_contacts.class);
+
+             }
+         });
+
     }
 }
