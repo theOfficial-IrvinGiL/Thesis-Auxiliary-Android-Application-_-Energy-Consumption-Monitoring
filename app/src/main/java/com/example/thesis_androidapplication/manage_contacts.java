865eb640eb4c6cb33c03c4870c4c5d71dca2cff4
Irvin Gil <theofficial.irvingil@gmail.com>
2021-09-20 10:34:23 +0800

    tried to make database and add contacts

--- app/src/main/java/com/example/thesis_androidapplication/manage_contacts.java
+++ app/src/main/java/com/example/thesis_androidapplication/manage_contacts.java
@@ -1,6 +1,7 @@
 package com.example.thesis_androidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +10,10 @@ import android.view.View;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class manage_contacts extends AppCompatActivity {
+RecyclerView recyclerview;
+    FloatingActionButton toAddcontacts;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +21,14 @@ public class manage_contacts extends AppCompatActivity {
         setContentView(R.layout.activity_manage_contacts);
 
 
-        //code for floating action button
-        /*
-        * color of the floating action button can be modified on the colors.xml
-        * */
 
 
 
-        FloatingActionButton toAddcontacts = findViewById(R.id.addContactsFAbutton);
+        toAddcontacts = findViewById(R.id.addContactsFAbutton);
+        recyclerview = findViewById(R.id.recyclerView);
+
+
+
         toAddcontacts.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
