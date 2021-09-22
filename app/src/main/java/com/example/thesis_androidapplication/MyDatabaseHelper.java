ede5341fbc2527012c8990fe50d04af5f886a645
Irvin Gil <theofficial.irvingil@gmail.com>
2021-09-23 07:20:18 +0800

    Resolved issues: 1. empty name on adding contacts 2. inputed contact number will not be accepted when it is <11 and >10

--- app/src/main/java/com/example/thesis_androidapplication/MyDatabaseHelper.java
+++ app/src/main/java/com/example/thesis_androidapplication/MyDatabaseHelper.java
@@ -15,7 +15,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "AuxiliaryApp.db";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String CONTACTS_TABLE_NAME = "user_contacts";
+
+    //for contacts database information
+    private static final String CONTACTS_TABLE_NAME = "Contacts_Table";
     private static final String CONTACTS_COLUMN_ID = "user_id";
     private static final String CONTACTS_USERNAME = "user_name";
     private static final String CONTACTS_USER_CONTACTS = "user_contact";
@@ -31,9 +33,18 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        /*Explanation on why contacts will be saved as text?
+        *
+        * encountered problem in saving integer because of the datatype memory limit
+        * so i made that the contact number will be saved as text and the error trapping
+        * will be programmed to detect contact as an integer =irvin.gil=
+        * */
+
+        //database for contacts
         String query = "CREATE TABLE " + CONTACTS_TABLE_NAME +
                 " (" + CONTACTS_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + CONTACTS_USERNAME + " TEXT, " + CONTACTS_USER_CONTACTS + " INTEGER)";
+                + CONTACTS_USERNAME + " TEXT, " + CONTACTS_USER_CONTACTS + " TEXT)";
 
         db.execSQL(query);
 
@@ -46,7 +57,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     //add contact
-    void add_thiscontact(String username, Integer contact){
+    void add_thiscontact(String username, String contact){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();  //initialize content value
 
@@ -76,6 +87,16 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     }
 
+   /* Cursor searchDBforExistingContact(String thisContact){
+        String query = "SELECT * FROM "+CONTACTS_TABLE_NAME+" WHERE "+CONTACTS_USER_CONTACTS+" = " +thisContact;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }*/
+
     void updateData (String row_id, String name, String contactNumber){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();  //initialize content value
@@ -101,6 +122,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             Toast.makeText(context, "Contact deleted successfully!", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+
 
 
 }
