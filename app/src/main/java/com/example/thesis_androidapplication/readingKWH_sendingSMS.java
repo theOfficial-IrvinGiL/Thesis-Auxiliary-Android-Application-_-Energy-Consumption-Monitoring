788954d427b787f49979bd2ddbded0ae30126711
Irvin Gil <theofficial.irvingil@gmail.com>
2021-10-03 13:44:27 +0800

    made the second update activity work with its listview and can now also choose the contacts and automatically display it into the edittext

--- app/src/main/java/com/example/thesis_androidapplication/readingKWH_sendingSMS.java
+++ app/src/main/java/com/example/thesis_androidapplication/readingKWH_sendingSMS.java
@@ -2,17 +2,17 @@ package com.example.thesis_androidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,10 +26,20 @@ public class readingKWH_sendingSMS extends AppCompatActivity {
     /* initialized to be used for displaying the cost calculated from the last activity*/
     protected TextView displayCost;
 
+   TextInputLayout display_name, display_contact;
+
+
     /* used for getting the date from the Intent on last activity*/
     protected String reading_date , energy_cost;
 
     List <String> list =new ArrayList<>();
+
+    /*holder variable for the chosen values to send sms*/
+    private String chosen_name, chosen_contact;
+
+    /*button object initialized*/
+    Button calculate_and_send;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,9 @@ public class readingKWH_sendingSMS extends AppCompatActivity {
 
         /*assigning the id of textview*/
         displayCost = findViewById(R.id.display_cost_textview);
+        display_name = findViewById(R.id.view_name);
+        display_contact = findViewById(R.id.view_contact);
+        calculate_and_send = findViewById(R.id.calcAndSend_SMS);
 
         /*initializing bundle*/
         Bundle bundle = getIntent().getExtras();
@@ -63,17 +76,38 @@ public class readingKWH_sendingSMS extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Selected: "+position, Toast.LENGTH_SHORT).show();
+                /*get the index position number of the chosen item in the listview*/
+                chosen_name = list.get(position);
+                display_name.getEditText().setText(chosen_name); //display the name in the textview
+
+                /*display toast message*/
+                Toast.makeText(readingKWH_sendingSMS.this, "Selected: "+chosen_name, Toast.LENGTH_SHORT).show();
+
+                /*code to retrieve the corresponding contact number of the
+                * chosen name in the database*/
+                Cursor cursor = myDB.loadUserContactNumber(chosen_name);
+                if(cursor.getCount() == 0){  //if the cursor is empty, display toast message
+                    Toast.makeText(getApplicationContext(), "No contact names retrieved", Toast.LENGTH_SHORT).show();
+                }else{
+                    while(cursor.moveToNext()){ //if cursor has data then add to the variable to be displayed
+                        chosen_contact = cursor.getString(0);
+                    }
+                }
+
+                /*display the contact number on the editText*/
+                display_contact.getEditText().setText(chosen_contact);
             }
         });
-
-
         /*listview code ends here*/
 
 
+        /*button onclickListener*/
+        calculate_and_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-
-
+            }
+        });
 
 
 
@@ -95,4 +129,5 @@ public class readingKWH_sendingSMS extends AppCompatActivity {
         }
 
     }
+
 }
